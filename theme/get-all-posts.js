964be434da68a30009c9041b22f03e152a5538a4
Thanish Nizam <mnmtanish@gmail.com>
2020-12-25 19:23:38 +0100

    Include the post link on meta

--- theme/get-all-posts.js
+++ theme/get-all-posts.js
@@ -1,6 +1,7 @@
 const r = require.context('../pages/blog/', true, /\.mdx$/);
 
-export const posts = r.keys().map((fileName) => ({
-  link: fileName.substr(1).replace(/\/index\.mdx$/, ''),
-  meta: r(fileName).meta,
-}));
+export const posts = r.keys().map((fileName) => {
+  const link = fileName.substr(1).replace(/\/index\.mdx$/, '');
+  const meta = { ...r(fileName).meta, link };
+  return { link, meta };
+});
