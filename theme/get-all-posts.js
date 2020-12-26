62c93a9ec21290ad9add0b31c6d1168af2383a80
Thanish Nizam <mnmtanish@gmail.com>
2020-12-26 21:09:06 +0100

    Import old content from Hugo

--- theme/get-all-posts.js
+++ theme/get-all-posts.js
@@ -1,7 +1,10 @@
 const r = require.context('../pages/blog/', true, /\.mdx$/);
 
-export const posts = r.keys().map((fileName) => {
-  const link = fileName.substr(1).replace(/\/index\.mdx$/, '');
-  const meta = { ...r(fileName).meta, link };
-  return { link, meta };
-});
+export const posts = r
+  .keys()
+  .map((fileName) => {
+    const link = fileName.substr(1).replace(/\/index\.mdx$/, '');
+    const meta = { ...r(fileName).meta, link };
+    return { link, meta };
+  })
+  .sort((a, b) => (a.meta.date > b.meta.date ? -1 : 1));
